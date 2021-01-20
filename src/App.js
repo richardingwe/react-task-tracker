a20a064691ca9807d2771db5e5e314e9ded2c948
Richard Ingwe <richardingwe5@gmail.com>
2021-01-20 01:00:35 +0100

    added addtask function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import AddTask from './components/AddTask';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 
@@ -24,6 +25,12 @@ function App() {
     }
   ]
   );
+  const addTask = (task) => {
+    const id = Math.floor(Math.random()* 10000) + 1
+
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
   const deleteTask = (id) => {
     setTasks(tasks.filter(task => task.id !== id))
   }
@@ -33,6 +40,7 @@ function App() {
   return (
     <div className="container">
       <Header/>
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? (
 
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
